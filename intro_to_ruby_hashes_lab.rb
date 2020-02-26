34098b3b10db2bb12d7a30c364c9c729f5dba490
Ras azzab <ras.dinho7@gmail.com>
2020-02-26 20:59:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,12 +16,12 @@ def monopoly_with_third_tier
   
 {:railroads => {
   :pieces => 4, 
+    :rent_in_dollars => {
   :names => {
     :reading_railroad =>{},
     :pennsylvania_railroad =>{},
     :b_and_o_railroad =>{},
     :shortline =>{}},
-    :rent_in_dollars => {
       :one_piece_owned => 25 ,
       :two_pieces_owned => 50,
       :three_pieces_owned => 100,
