e716ab0c9d5ccd2c1aa2ebc882a9dee18977ed6c
Ras azzab <ras.dinho7@gmail.com>
2020-02-26 19:42:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,6 +11,7 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 	{:railroads => {:pieces => 4}}
+}
 end
 
 def monopoly_with_third_tier
